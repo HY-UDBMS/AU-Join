f54458155dfc8716e92d22ec0a101910f9341e63
Paddy Xu <xupaddy@gmail.com>
2019-07-12 17:23:34 +0300

    Update help

--- src/main/kotlin/fi/helsinki/cs/udbms/util/RuntimeParameters.kt
+++ src/main/kotlin/fi/helsinki/cs/udbms/util/RuntimeParameters.kt
@@ -41,7 +41,7 @@ class RuntimeParameters(parser: ArgParser) {
                 args,
                 helpFormatter = DefaultHelpFormatter(
                     epilogue = """
-                    Example: ./AU-Join --taxonomy tax.txt --synonym syn.txt -c3 -o result.csv 0.9 list1.txt list2.txt
+                    Example: ./AU-Join --taxonomy tax.txt --synonym syn.txt --jaccard 3 -c3 -oresult.csv 0.9 list1.txt list2.txt
                 """.trimIndent()
                 )
             ).parseInto(::RuntimeParameters)
